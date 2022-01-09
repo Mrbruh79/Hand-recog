27ecda24b5dfe27bdcbdde0f6b2ba88511a7f012
Arnav Bansal <arnavb1562002@gmail.com>
2022-01-09 16:19:47 +0530

    made function to make a coordinate array

--- cascade.py
+++ cascade.py
@@ -11,18 +11,6 @@ import glob
 import scipy.io
 from mat4py import loadmat
 
-a =  loadmat(r"C:\Users\Restandsleep\Desktop\VIT\Personal\hand_dataset\training_dataset\training_data\annotations\Buffy_10.mat")
-print(a)
-print("\n")
-b = list(a.values())
-print("\n")
-print(b)
-print("\n")
-print((b[0][1]))
-print("\n")
-c = list(b[0][1].values())
-print("\n")
-print(c[1])
 
 
 import tensorflow as tf
@@ -35,19 +23,37 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop,Adam
 import tensorflow_io as tfio
-import tensorflow_datasets as tfds
 
 
 
 #Test space
 
+# a =  loadmat(r"C:\Users\Restandsleep\Desktop\VIT\Personal\hand_dataset\training_dataset\training_data\annotations\Buffy_10.mat")
+# print(a)
+# print("\n")
+# b = list(a.values())
+# print("\n")
+# print(type(b[0]))
+# print("\n")
+# # print((b[0][1]))
+# print("\n")
+# c = list(b[0].values())
+# print("\n")
+# # print(c)
 
 
+# a =  loadmat(r"C:\Users\Restandsleep\Desktop\VIT\Personal\hand_dataset\training_dataset\training_data\annotations\VOC2010_998.mat")
+# print(a)
 
-
-
-
-
+# b = list(a.values())
+# print("\n")
+# print(b[0])
+# print("\n")
+# print((b[0][1]))
+# print("\n")
+# c = list(b[0][1].values())
+# print("\n")
+# print()
 
 
 #Importing data
@@ -86,13 +92,30 @@ test_dir_val = glob.glob(os.path.join(test_dir_val, "*.mat"))
 
 #mat  data process function
 def getvalues(data):
+    b = 0
     b = list(data.values())
     i = len(b[0])
-    hands = i
     coor = []
-    for x in range(0,i):
-        c = list(b[0][x].values())
-        
+    
+    if type(b[0]) == list:
+        for x in range(0,i-1):
+            y = list(b[0][x].values())
+            coor.append(y[0:4])
+            # print(y , "\n")
+            if(len(y)>4):
+                coor.append(y[4])   
+            else:
+                coor.append(0)
+    else:
+        y = list(b[0].values())
+        coor.append(y[0:4])      
+        if(len(y)>4):
+            coor.append(y[4])   
+        else:
+            coor.append(0)
+    return coor 
+
+
 
 #inporting location data
 train_data = []
@@ -101,7 +124,10 @@ validation_data = []
 
 for filename in train_dir_val:
     data = loadmat(filename)
+    print(filename)
+    data = getvalues(data)
     train_data.append(data)
+   
     
 for filename in validation_dir_val:
     data = loadmat(filename)
@@ -109,13 +135,15 @@ for filename in validation_dir_val:
 for filename in test_dir_val:
     data = loadmat(filename)
 
-train_ds = tf.data.Dataset.from_tensor_slices((train_dir,train_data))
+# train_ds = tf.data.Dataset.from_tensor_slices((train_dir,train_data))
 
 
-for filename in train_dir:    
-    img = cv.imread(filename)
+# for filename in train_dir:    
+#     img = cv.imread(filename)
 
 
+print(train_data)
+print("\n")
 
 
 # for i in train_data:
